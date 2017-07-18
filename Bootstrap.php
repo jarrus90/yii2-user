d966ec83ff519743b20cb2a2b7b1c815bf853fe9
Vladimir Stovbchatij <unreality90@mail.ru>
2017-07-18 10:46:06 +0300

    Fix admin menu

--- Bootstrap.php
+++ Bootstrap.php
@@ -84,7 +84,15 @@ class Bootstrap implements BootstrapInterface {
                 $app->params['admin']['menu']['user'] = function() use($module) {
                     return $module->getAdminMenu();
                 };
-                
+
+                $app->params['admin']['menu']['login'] = function() {
+                    return [
+                        'label' => Yii::t('user', 'Log in'),
+                        'icon' => '<i class="fa fa-sign-out"></i>',
+                        'url' => '/user/security/login'
+                    ];
+                };
+
                 $app->params['admin']['menu']['logout'] = function() {
                     return [
                         'label' => Yii::t('user', 'Logout'),
